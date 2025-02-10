d136d7f9157c4e3733cd7ac37474b2005b9d74e3
DurgaShree-V <165573697+DurgaShree-V@users.noreply.github.com>
2025-02-10 09:15:50 +0530

    Update mul_nxn.cpp

--- mul_nxn.cpp
+++ mul_nxn.cpp
@@ -5,28 +5,24 @@ using namespace std;
 
 void multiply_A_and_B(int **a, int **b, int **result, int n) {
     for (size_t i = 0; i < n; i++) {  
-        for (size_t j = 0; j < n; j++) {  
-            size_t vl = __riscv_vsetvl_e32m1(n);  
-            // cout << vl << endl;
-            vint32m1_t sum = __riscv_vmv_v_x_i32m1(0, vl);  
+        for (size_t j = 0; j < n; j ++) {  
+            size_t vl = __riscv_vsetvl_e32m2(n - j);  
+
+            vint32m2_t sum = __riscv_vmv_v_x_i32m2(0, vl); 
 
             for (size_t k = 0; k < n; k++) {  
-                vint32m1_t va = __riscv_vmv_v_x_i32m1(a[i][k], vl);  
-                vint32m1_t vb = __riscv_vle32_v_i32m1(&b[k][j], vl); 
-
-                vint32m1_t mul = __riscv_vmul_vv_i32m1(va, vb, vl);  
-                sum = __riscv_vadd_vv_i32m1(sum, mul, vl);  
+                vint32m2_t va = __riscv_vmv_v_x_i32m2(a[i][k], vl);  
+                vint32m2_t vb = __riscv_vle32_v_i32m2(&b[k][j], vl);  
+                sum = __riscv_vadd_vv_i32m2(sum, __riscv_vmul_vv_i32m2(va, vb, vl), vl);
             }
-            int temp[8];  
-            __riscv_vse32_v_i32m1(temp, sum, vl);  
-
-            result[i][j] = temp[0]; 
+            __riscv_vse32_v_i32m2(&result[i][j], sum, vl); 
         }
     }
 }
 
+
 int main() {
-    int n = 3;
+    int n = 16;
     int **a = new int*[n];
     int **b = new int*[n];
     int **result = new int*[n];
