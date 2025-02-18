889c06e42b36f9d2c597cd71b39e716d353f725e
DurgaShree-V <165573697+DurgaShree-V@users.noreply.github.com>
2025-02-18 15:37:30 +0530

    Update rotate_image.cpp

--- rotate_image.cpp
+++ rotate_image.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 #include <riscv_vector.h>
 
-void print(float matrix[16][16], size_t n) {
+void print(float *matrix, size_t n) {
     for (size_t i = 0; i < n; ++i) {
         for (size_t j = 0; j < n; ++j) {
-            std::cout << matrix[i][j] << " ";
+            std::cout << matrix[i*n + j] << " ";
         }
         std::cout << std::endl;
     }
 }
 
-void rotate(float src[16][16], float dst[16][16], size_t n) 
+void rotate(float *src, float *dst, size_t n) 
 {
     for (int i = n - 1,j= 0; i >= 0; i--,j++) {  
         
         size_t avl = n;
-        float* row_src = src[i];
-        float* row_dst = &dst[0][j];
+        float* row_src = src + i*n;
+        float* row_dst = dst + j;
 
         while (avl > 0) {
             size_t vl = __riscv_vsetvl_e32m1(avl);
@@ -32,9 +32,13 @@ void rotate(float src[16][16], float dst[16][16], size_t n)
 }
 
 int main() {
-    size_t n = 16;
+    size_t n;
     float count = 1.0;
-    float a[16][16], result[16][16];
+    
+    std::cout << "Enter the value of n in nxn: ";
+    std::cin >> n;
+
+    float a[n][n], result[n][n];
 
     std::cout << "The elements of Matrix A:" << std::endl;
     for (int i = 0; i < n; i++) {
@@ -44,12 +48,12 @@ int main() {
     }
 
     std::cout << "Original Matrix:" << std::endl;
-    print(a, n);
+    print(&a[0][0], n);
 
-    rotate(a,result,n);
+    rotate(&a[0][0], &result[0][0], n);
 
     std::cout << "\nOutput Matrix:" << std::endl;
-    print(result, n);
+    print(&result[0][0], n);
 
     return 0;
 }
